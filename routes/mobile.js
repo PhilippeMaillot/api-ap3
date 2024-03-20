e2baf390c6a8b7cc4c44afb90e50e96ec9d75859
PhilippeMaillot <119845108+PhilippeMaillot@users.noreply.github.com>
2024-03-20 15:17:46 +0100

    Change mobileUser Route

--- routes/mobile.js
+++ routes/mobile.js
@@ -2,17 +2,7 @@ const express = require("express");
 const router = express.Router();
 const mobileController = require("../controllers/mobileController");
 
-router.get('/', (req, res) => {
-    db.query('SELECT * FROM mobile_user', (err, results) => {
-      if (err) {
-        console.error('Erreur lors de la récupération des données : ' + err.message);
-        res.status(500).json({ err: 'Erreur serveur' });
-        return;
-      }
-  
-      res.status(200).json(results);
-    });
-  });
+router.get("/", tournamentController.getAllUsers);
 
 router.post("/add", mobileController.addUser);
 
